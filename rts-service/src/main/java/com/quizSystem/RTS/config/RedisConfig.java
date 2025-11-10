b908b6eec3be083cafe18db428897bd2a5b5ad0f
cormacGreaney <22352228@studentmail.ul.ie>
2025-11-10 04:14:31 +0000

    wire RTS (Redis), QMS (MySQL), gateway routes, docker config for rest of services

--- rts-service/src/main/java/com/quizSystem/RTS/config/RedisConfig.java
+++ rts-service/src/main/java/com/quizSystem/RTS/config/RedisConfig.java
@@ -1,31 +1,31 @@
 package com.quizSystem.RTS.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;            
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory; 
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        // connects to localhost:6379
-        return new LettuceConnectionFactory("localhost", 6379);
+    public RedisConnectionFactory redisConnectionFactory(
+            @Value("${spring.data.redis.host:localhost}") String host,
+            @Value("${spring.data.redis.port:6379}") int port) {
+        // Use Spring properties (overridden by env in Docker)
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
-
-        // make keys and values readable strings
-        template.setKeySerializer(new org.springframework.data.redis.serializer.StringRedisSerializer());
-        template.setValueSerializer(new org.springframework.data.redis.serializer.StringRedisSerializer());
-
+        template.setConnectionFactory(connectionFactory);
+        // Minimal serializers; adjust later if you store complex objects
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
         return template;
     }
-
 }
-
-
